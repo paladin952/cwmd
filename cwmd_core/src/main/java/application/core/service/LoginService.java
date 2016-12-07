12d59bdd7073ba06741a4bcf3eceffc20b161b94
Clapa Lucian <lucian@dealdash.com>
2016-12-07 16:07:49 +0200

    add login function in service

--- cwmd_core/src/main/java/application/core/service/LoginService.java
+++ cwmd_core/src/main/java/application/core/service/LoginService.java
@@ -1,9 +1,12 @@
 package application.core.service;
 
+import application.core.model.User;
 import application.core.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by clapalucian on 12/7/16.
@@ -17,9 +20,15 @@ public class LoginService {
 
     public boolean isLoggedIn(String username, String password) {
 
+        List<User> users = userRepo.findAll();
 
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)){
+                return true;
+            }
+        }
 
-        return true;
+        return false;
     }
 
 }
