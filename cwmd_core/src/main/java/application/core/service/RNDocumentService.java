c3dfb90f55172159ff61b979b23b77009b9c271e
dana.ciubancan <dana.ciubancan0914@gmail.com>
2016-12-17 11:56:57 +0200

    RN: upload file + save to disk&db done

--- cwmd_core/src/main/java/application/core/service/RNDocumentService.java
+++ cwmd_core/src/main/java/application/core/service/RNDocumentService.java
@@ -4,6 +4,7 @@ package application.core.service;
 import application.core.model.rn.*;
 import application.core.repository.RNDocumentRepository;
 import application.core.utils.UserUtil;
+import com.aspose.cells.Cell;
 import com.aspose.cells.Cells;
 import com.aspose.cells.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,26 +44,48 @@ public class RNDocumentService {
         rnDocument.setPersonalFunds(personalFunds);
 
         RNResearch rnResearch = new RNResearch();
-        rnResearch.setNationalFunds1Identification(cells.get("B8").getStringValue());
-        rnResearch.setNationalFunds1(cells.get("D8").getFloatValue());
-        rnResearch.setNationalFunds2Identification(cells.get("B9").getStringValue());
-        rnResearch.setNationalFunds2(cells.get("D9").getFloatValue());
-        rnResearch.setTernaryContractsIdentification(cells.get("B10").getStringValue());
-        rnResearch.setTernaryContracts(cells.get("D10").getFloatValue());
+
+        Cell b8 = cells.get("B8");
+        if (b8.getValue() != null) {
+            rnResearch.setNationalFunds1Identification(b8.getStringValue());
+            rnResearch.setNationalFunds1(cells.get("D8").getFloatValue());
+        }
+        Cell b9 = cells.get("B9");
+        if (b9.getValue() != null) {
+            rnResearch.setNationalFunds2Identification(b9.getStringValue());
+            rnResearch.setNationalFunds2(cells.get("D9").getFloatValue());
+        }
+        Cell b10 = cells.get("B10");
+        if (b10.getValue() != null) {
+            rnResearch.setTernaryContractsIdentification(b10.getStringValue());
+            rnResearch.setTernaryContracts(cells.get("D10").getFloatValue());
+        }
         rnDocument.setRnResearch(rnResearch);
 
         RNSponsors rnSponsors = new RNSponsors();
-        rnSponsors.setSponsorName(cells.get("B11").getStringValue());
-        rnSponsors.setAmount(cells.get("D11").getFloatValue());
+        Cell b11 = cells.get("B11");
+        if (b11.getValue() != null) {
+            rnSponsors.setSponsorName(b11.getStringValue());
+            rnSponsors.setAmount(cells.get("D11").getFloatValue());
+        }
         rnDocument.setRnSponsors(rnSponsors);
 
         RNOthers rnOthers = new RNOthers();
-        rnOthers.setStructuralFundsIdentification(cells.get("B13").getStringValue());
-        rnOthers.setStructuralFunds(cells.get("D13").getFloatValue());
-        rnOthers.setExternalFundingIdentification(cells.get("B14").getStringValue());
-        rnOthers.setExternalFunding(cells.get("D14").getFloatValue());
-        rnOthers.setOtherIdentification(cells.get("B15").getStringValue());
-        rnOthers.setOther(cells.get("D15").getFloatValue());
+        Cell b13 = cells.get("B13");
+        if (b13.getValue() != null) {
+            rnOthers.setStructuralFundsIdentification(b13.getStringValue());
+            rnOthers.setStructuralFunds(cells.get("D13").getFloatValue());
+        }
+        Cell b14 = cells.get("B14");
+        if (b14.getValue() != null) {
+            rnOthers.setExternalFundingIdentification(b14.getStringValue());
+            rnOthers.setExternalFunding(cells.get("D14").getFloatValue());
+        }
+        Cell b15 = cells.get("B15");
+        if (b15.getValue() != null) {
+            rnOthers.setOtherIdentification(b15.getStringValue());
+            rnOthers.setOther(cells.get("D15").getFloatValue());
+        }
         rnOthers.setAdvancePayment(cells.get("D16").getFloatValue());
         rnDocument.setRnOthers(rnOthers);
 
@@ -71,13 +94,18 @@ public class RNDocumentService {
         int i = 6;
         while (i <= 14) {
             RNProduct rnProduct = new RNProduct();
-            rnProduct.setNrCrt(cells.get("G" + i).getIntValue());
+            Cell nrCrt = cells.get("G" + i);
+            if (nrCrt.getValue() == null) {
+                break;
+            }
+            rnProduct.setNrCrt(nrCrt.getIntValue());
             rnProduct.setName(cells.get("H" + i).getStringValue());
             rnProduct.setCodCPV(cells.get("I" + i).getStringValue());
             rnProduct.setUm(cells.get("J" + i).getStringValue());
             rnProduct.setQuantity(cells.get("K" + i).getIntValue());
             rnProduct.setPricePerUnit(cells.get("L" + i).getFloatValue());
             rnProduct.setTotalPrice(cells.get("M" + i).getFloatValue());
+            rnProduct.setRnDocument(rnDocument);
             products.add(rnProduct);
 
             i++;
--- cwmd_web/src/main/java/application/web/controller/RNDocumentController.java
+++ cwmd_web/src/main/java/application/web/controller/RNDocumentController.java
@@ -20,27 +20,21 @@ public class RNDocumentController {
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public Integer uploadRN(@RequestParam("file") MultipartFile file) throws Exception {
         Workbook workbook = null;
-        Integer docId = null;
         try {
             workbook = new Workbook(file.getInputStream());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        Cell cell = workbook.getWorksheets().get(0).getCells().get("B8");
+        if (workbook != null) {
+            Cell cell = workbook.getWorksheets().get(0).getCells().get("B8");
 
-        System.out.println("cell.getValue().toString() = " + cell.getValue().toString());
+            System.out.println("cell.getValue().toString() = " + cell.getValue().toString());
 
-        rnDocumentService.saveDocumentOnDisk(workbook, file.getOriginalFilename());
-        return rnDocumentService.saveDocumentInDB(workbook, file.getOriginalFilename());
+            rnDocumentService.saveDocumentOnDisk(workbook, file.getOriginalFilename());
+            return rnDocumentService.saveDocumentInDB(workbook, file.getOriginalFilename());
+        }
 
-//        drDocumentService.saveDocumentOnServer(document, file.getOriginalFilename());
-//        if (documentId == null) {
-//            documentId = drDocumentService.saveDocumentInDBFirstPart(document, file.getOriginalFilename());
-//            docId = documentId;
-//        } else {
-//            drDocumentService.saveDocumentInDBSecondPart(document, documentId);
-//            documentId = null;
-//        }
+        return null;
     }
 }
