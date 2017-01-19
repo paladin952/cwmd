94f0caa07c09e69e6d3a8155e1b0e48655b9020b
Cioba Catalin Adrian <cioba.catalin@yahoo.com>
2017-01-19 14:58:32 +0200

    more documentation

--- cwmd_core/src/main/java/application/core/utils/UserUtil.java
+++ cwmd_core/src/main/java/application/core/utils/UserUtil.java
@@ -1,5 +1,7 @@
 package application.core.utils;
 
+import application.core.model.User;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
@@ -7,7 +9,15 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Utility class for {@link User}
+ */
 public class UserUtil {
+
+    /**
+     * Returns the username of the user making the request.
+     * @return The username of the user
+     */
     public static String getCurrentUsername(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
         List<Cookie> cookieList = Arrays.asList(cookies);
--- cwmd_core/src/main/java/application/core/validator/UserValidator.java
+++ cwmd_core/src/main/java/application/core/validator/UserValidator.java
@@ -5,13 +5,28 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+/**
+ * Validator for {@link User}
+ */
 @Component
-public class UserValidator implements Validator{
+public class UserValidator implements Validator {
+
+    /**
+     * Checks if the Validator can validate a Class
+     *
+     * @param clazz The class to be validated
+     * @return True if it can be validated by this validator, False otherwise
+     */
     @Override
     public boolean supports(Class<?> clazz) {
         return User.class.equals(clazz);
     }
 
+    /**
+     * Validate a given object
+     *
+     * @param target The target class to be validated
+     */
     @Override
     public void validate(Object target, Errors errors) {
         User user = (User) target;
@@ -20,6 +35,11 @@ public class UserValidator implements Validator{
         validatePassword(user, errors);
     }
 
+    /**
+     * Validates Usernamae for a user
+     *
+     * @param user The user to be validated
+     */
     private void validateUsername(User user, Errors errors) {
         String name = user.getUsername();
         if (name.length() < 5 || name.length() > 255) {
@@ -27,6 +47,11 @@ public class UserValidator implements Validator{
         }
     }
 
+    /**
+     * Validates Password for a user
+     *
+     * @param user The user to be validated
+     */
     private void validatePassword(User user, Errors errors) {
         String password = user.getPassword();
         if (password.length() < 5 || password.length() > 255) {
